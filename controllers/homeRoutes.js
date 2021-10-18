cd8aafac001c7bae3a4368c5d1db87c57c2c3275
JacksonWhite4725 <jawh4725@colorado.edu>
2021-10-17 21:45:44 -0600

    Added some routes for home, fixed starting node server.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,20 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const {User, Blog, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async(req, res) => {
+    try {
+        const blogData = await Blog.findAll({include: [{ model: User }]});
+        const blogBody = blogData.map((blog) => blog.get({ plain: true }));
+
+        res.render('homepage', {
+            blogBody,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
